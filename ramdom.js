6d590f27f00728a4282949f1e4dcbde12db1c3ba
ricardoayala <rirkdo.ayala@gmail.com>
2015-07-17 14:11:25 -0500

    primer commit

---
+++ ramdom.js
@@ -0,0 +1,10 @@
+//devuelve numero aleatorio entre 0 y 1
+var numero = Math.random();
+if (numero <= 0.05)
+{
+	console.log('\n' + numero + 'Menor que 0.05 \n' );
+}
+else
+{
+	console.log('\n' + numero + 'Mayor que 0.05 \n' );
+}
